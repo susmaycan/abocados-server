66a00f37257890fbddc4e7cd720e7d60a5556909
asdy <adoptdontbuy2019@gmail.com>
2020-09-02 12:09:22 +0200

    Add cors and MongoDB connection

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-let cors = require('cors')
+var cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse application/json
 app.use(bodyParser.json())
-app.use(cors())
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+const uri = process.env.mongodburi
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database")
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err)
+    process.exit()
+})
+
+app.use(cors())
 require('./app/routes/recipe.routes.js')(app)
 
 const PORT = process.env.PORT || 3001
 
-// listen for requests
+
 app.listen(PORT, () => {
     console.log("Server is listening on port 3001")
 })
